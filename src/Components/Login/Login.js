e1ae7cc69fcacbcd79e6039b33839faa5ae6e4cb
Vahab Vahabzada <106394551+vahabvahabzada@users.noreply.github.com>
2022-12-25 17:12:02 +0400

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,18 @@ import KursSec from "../Home/KursSec";
 import MenuCategory from "../Home/MenuCategory";
 import "./login.css";
 export default function Login(){
+    const loginController=()=>{
+        var enteredEmail=document.getElementById("email").value;
+        console.log(localStorage.getItem("email"));
+        var enteredPassword=document.getElementById("password").value;
+        console.log(localStorage.getItem("password"));
+        if(enteredEmail===localStorage.getItem("email") && enteredPassword===localStorage.getItem("password")){
+            alert("Welcome "+enteredEmail.substring(0,enteredEmail.indexOf("@"))+"!");
+        }
+        else{
+            alert("Failed to login! Please try again!");
+        }
+    }
     return (<div className="login">
     {/*<h1>Here is Login Page</h1>*/}
     <MenuCategory/>
@@ -17,14 +29,14 @@ export default function Login(){
             <p id="signinmessage">Sign into your account</p>
             <form>
             <div className="email">
-                <input type="email" placeholder="Email address"/>
+                <input type="email" placeholder="Email address" id="email"/>
             </div>
             <div className="password">
-                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Password" id="password"/>
             </div>
             <a href="#!" id="forgot">Forgot password?</a>
             </form>
-            <button type="submit" id="signinbutton">LOGIN</button>
+            <button type="submit" id="signinbutton" onClick={loginController}>LOGIN</button>
             <p id="continue">...or continue with...</p>
             <div id="signinwith">
               <a href="https://accounts.google.com/"><div id="google"></div></a>
