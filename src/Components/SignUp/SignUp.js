44367ed7e0639297b4604706ba7d5e1ebc3d1d38
Vahab Vahabzada <106394551+vahabvahabzada@users.noreply.github.com>
2022-12-25 17:09:05 +0400

    Update SignUp.js

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -5,6 +5,13 @@ import Footer from "../Home/Footer";
 import "./signup.css"
 import { Link } from "react-router-dom";
 export default function SignUp(){
+    const signupController=()=>{
+        var email=document.getElementById("email").value;
+        localStorage.setItem("email",email);
+        var password=document.getElementById("password").value;
+        localStorage.setItem("password",password);
+        alert("Welcome,you have succesfully signed up!");
+    }
     return (<div className="signup">
         <MenuCategory/>
         <KursSec/>
@@ -16,14 +23,14 @@ export default function SignUp(){
             <p id="signupmessage">Sign into your account</p>
             <form>
             <div className="email">
-                <input type="email" placeholder="Email address"/>
+                <input type="email" placeholder="Email address" id="email"/>
             </div>
             <div className="password">
-                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Password" id="password"/>
             </div>
             <p id="alreadyhave">Already have an account?<Link to="/login">Login</Link></p>
             </form>
-            <button type="submit" id="signupbutton">Sign Up</button>
+            <button type="submit" id="signupbutton" onClick={signupController}>Sign Up</button>
             <p id="continue">...or continue with...</p>
             <div id="signupwith">
                 <a href="https://accounts.google.com/"><div id="google"></div></a>
